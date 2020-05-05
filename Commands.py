2a835ae29f6f5ccaea0bd16f3c7577b232b95c28
Zenahr Barzani <zenmatica@gmail.com>
2020-05-05 19:20:48 +0200

    added browser open command

--- Commands.py
+++ Commands.py
@@ -11,3 +11,6 @@ def standby(string):
     if(string == "shutdown"):
         print("putting computer into standby mode")
         os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0") # put into standby mode. If doesn't work, turn off hibernation feature on machine.
+
+def open_browser(command):
+    wb.open_new("http://www.google.com")
