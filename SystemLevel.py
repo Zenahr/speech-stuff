223e0abf80533c1ae7f23320faefc03d4f365a6d
Zenahr Barzani <zenmatica@gmail.com>
2020-05-05 19:18:43 +0200

    added standby function

--- SystemLevel.py
+++ SystemLevel.py
@@ -2,7 +2,12 @@ import webbrowser as wb
 import os
 
 
-def function(string):
+def shutdown(string):
     if(string == "shutdown"):
         print("shutting down computer")
         os.system("shutdown /s /f /t 3") # forces pc shutdown. Closes all apps automatically.
+
+def standby(string):
+    if(string == "shutdown"):
+        print("putting computer into standby mode")
+        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0") # put into standby mode. If doesn't work, turn off hibernation feature on machine.
