79239a1d2e0d4cfcbc2ad5991422f32dd303690d
Zenahr Barzani <zenmatica@gmail.com>
2020-05-05 18:21:38 +0200

    finished poc

---
+++ demo.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+import webbrowser as wb
+
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+
+with sr.Microphone() as source:
+        print("[search edureka: search youtube]")
+        print("speak now")
+        audio = r3.listen(source)
+
+if "gern geschehen" in r2.recognize_google(audio, language="de-DE"):
+    print("detected")
+    r2 = sr.Recognizer()
+    url = "https://www.google.com/"
+
+    with sr.Microphone as source:
+        print("search your query!")
+        audio = r2.listen(source)
+
+        try:
+            get = r2.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print("error")
+        except sr.RequestError as e:
+            print("failed".format(e))
